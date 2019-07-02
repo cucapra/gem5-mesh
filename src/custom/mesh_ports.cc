94de3c7b4768a57273e96a053e2e5cf5f0f8c101
pbb59 <pbb59@cornell.edu>
2019-07-02 11:30:02 -0400

    factor out mesh ports from cpu

---
+++ src/custom/mesh_ports.cc
@@ -0,0 +1,223 @@
+#include "custom/mesh_ports.hh"
+#include "cpu/simple/timing.hh"
+
+
+/*----------------------------------------------------------------------
+ * Implement base port behavior
+ *--------------------------------------------------------------------*/ 
+
+TimingCPUSlavePort::TimingCPUSlavePort(const std::string& _name, int idx, TimingSimpleCPU* _cpu)
+        : SlavePort(_name, _cpu), idx(idx), cpu(_cpu)
+         //, retryRespEvent([this]{ sendRetryResp(); }, name())
+    { }
+
+void
+TimingCPUSlavePort::TickEvent::schedule(PacketPtr _pkt, Tick t)
+{
+    pkt = _pkt;
+    cpu->schedule(this, t);
+}
+
+TimingCPUMasterPort::TimingCPUMasterPort(const std::string& _name, int idx, TimingSimpleCPU* _cpu) : 
+  MasterPort(_name, _cpu), idx(idx), cpu(_cpu), retryRespEvent([this]{ sendRetryResp(); }, name()) {
+        
+}
+
+
+void
+TimingCPUMasterPort::TickEvent::schedule(PacketPtr _pkt, Tick t)
+{
+    pkt = _pkt;
+    cpu->schedule(this, t);
+}
+
+/*----------------------------------------------------------------------
+ * Define mesh master port behavior
+ *--------------------------------------------------------------------*/ 
+
+
+ToMeshPort::ToMeshPort(TimingSimpleCPU *_cpu, int idx)
+        : TimingCPUMasterPort(
+          _cpu->name() + ".mesh_out_port" + csprintf("[%d]", idx), 
+          idx, _cpu), tickEvent(_cpu), val(false), active(false), 
+          stalledPkt(nullptr)
+    { }
+
+// if you want to send a packet, used <MasterPort_Inst>.sendTimingReq(pkt);
+
+// after waiting for some delay, we finally do something with the recv
+// packet response
+void
+ToMeshPort::MOTickEvent::process()
+{
+    //cpu->completeIfetch(pkt);
+}
+
+// override what should happen when the packet we sent on this port has returned
+bool
+ToMeshPort::recvTimingResp(PacketPtr pkt)
+{
+    DPRINTF(Mesh, "Received mesh out response %#x\n", pkt->getAddr());
+    // we should only ever see one response per cycle since we only
+    // issue a new request once this response is sunk
+    assert(!tickEvent.scheduled());
+    // delay processing of returned data until next CPU clock edge
+    tickEvent.schedule(pkt, cpu->clockEdge());
+
+    return true;
+}
+
+// override what should happen when the packet we sent on this port failed to send
+void
+ToMeshPort::recvReqRetry()
+{
+    // we shouldn't get a retry unless we have a packet that we're
+    // waiting to transmit
+    /*assert(cpu->ifetch_pkt != NULL);
+    assert(cpu->_status == IcacheRetry);
+    PacketPtr tmp = cpu->ifetch_pkt;
+    if (sendTimingReq(tmp)) {
+        cpu->_status = IcacheWaitResponse;
+        cpu->ifetch_pkt = NULL;
+    }*/
+}
+
+void
+ToMeshPort::setVal(bool val) {
+  this->val = val;
+}
+
+// val/rdy, but also if we don't care about the connection we expect
+// both to be not val/rdy
+bool
+ToMeshPort::checkHandshake(){
+  BaseSlavePort *slavePort = &(getSlavePort());
+  if (FromMeshPort *slaveMeshPort = dynamic_cast<FromMeshPort*>(slavePort)) {
+    bool rdy = slaveMeshPort->getRdy();
+    // xnor
+    if ((val && rdy) || (!val && !rdy)) return true;
+    else return false;
+  }
+  else {
+    return false;
+  }
+}
+
+void
+ToMeshPort::tryUnblockNeighbor() {
+  BaseSlavePort *slavePort = &(getSlavePort());
+  if (FromMeshPort *slaveMeshPort = dynamic_cast<FromMeshPort*>(slavePort)) {
+    slaveMeshPort->tryUnblockCPU();
+  }
+}
+
+void
+ToMeshPort::failToSend(PacketPtr pkt) {
+  // assert not rdy
+  cpu->resetRdy();
+  
+  // store this packet to be used later
+  stalledPkt = pkt;
+}
+
+void
+ToMeshPort::beginToSend() {
+  // if there is a stalled packet lets send that now
+  if (stalledPkt) {
+    sendTimingReq(stalledPkt);
+    stalledPkt = nullptr;
+  }
+  
+  cpu->setValRdy();
+  
+  
+}
+
+/*----------------------------------------------------------------------
+ * Define mesh slave port behavior
+ *--------------------------------------------------------------------*/ 
+
+FromMeshPort::FromMeshPort(TimingSimpleCPU *_cpu, int idx)
+        : TimingCPUSlavePort(
+          _cpu->name() + ".mesh_in_port" + csprintf("[%d]", idx), 
+          idx, _cpu), tickEvent(_cpu), rdy(false), active(false)
+    { }
+
+// how to handle a request after waiting for some delay
+void
+FromMeshPort::MITickEvent::process(){
+  // cpu->dostuff();
+}
+
+bool 
+FromMeshPort::recvTimingReq(PacketPtr pkt) {
+  DPRINTF(Mesh, "Received mesh request %#x with data \n", pkt->getAddr());
+    // we should only ever see one response per cycle since we only
+    // issue a new request once this response is sunk
+    assert(!tickEvent.scheduled());
+    // delay processing of returned data until next CPU clock edge
+    tickEvent.schedule(pkt, cpu->clockEdge());
+
+    return true;
+}
+
+void
+FromMeshPort::recvRespRetry() {
+  // ?
+  assert(0);
+}
+
+void
+FromMeshPort::recvFunctional(PacketPtr pkt) {
+  // ? just call MITickEvent::proccess?
+  assert(0);
+}
+
+// might want to do bsg_manycore eva treament here for addressing
+AddrRangeList 
+FromMeshPort::getAddrRanges() const {
+  //return cpu->getAddrRanges();
+  return std::list<AddrRange>();
+}
+
+PacketPtr
+FromMeshPort::getPacket() {
+  PacketPtr ret = recvPkt;
+  recvPkt = nullptr;
+  return ret;
+}
+
+void
+FromMeshPort::setPacket(PacketPtr pkt) {
+  if (recvPkt != nullptr) {
+    DPRINTF(Mesh, "Overwrite packet %#x in port %d\n", recvPkt->getAddr(), idx);
+  }
+  
+  recvPkt = pkt;
+}
+
+void
+FromMeshPort::setRdy(bool rdy) {
+  this->rdy = rdy;
+}
+
+// val/rdy, but also if we don't care about the connection we expect
+// both to be not val/rdy
+bool
+FromMeshPort::checkHandshake(){
+  BaseMasterPort *masterPort = &(getMasterPort());
+  if (ToMeshPort *masterMeshPort = dynamic_cast<ToMeshPort*>(masterPort)) {
+    bool val = masterMeshPort->getVal();
+    // xnor
+    if ((val && rdy) || (!val && !rdy)) return true;
+    else return false;
+  }
+  else {
+    return false;
+  }
+}
+
+void
+FromMeshPort::tryUnblockCPU() { 
+  cpu->tryUnblock(); 
+}
---
+++ src/custom/mesh_ports.hh
@@ -0,0 +1,203 @@
+#ifndef __CUSTOM_MESH_PORTS_HH__
+#define __CUSTOM_MESH_PORTS_HH__
+
+/*
+ * Mesh ports used in TimingSimpleCPU
+ */ 
+
+
+#include "mem/port.hh"
+#include "mem/packet.hh"
+#include "sim/clocked_object.hh"
+
+//resolve circular deps
+class TimingSimpleCPU;
+
+// pbb implement mesh ports
+class TimingCPUMasterPort : public MasterPort
+{
+  public:
+
+    TimingCPUMasterPort(const std::string& _name, int idx, TimingSimpleCPU* _cpu);
+       /* : MasterPort(_name, _cpu), idx(idx), cpu(_cpu),
+          retryRespEvent([this]{ sendRetryResp(); }, name())
+    { }*/
+
+  protected:
+  
+    int idx;
+
+    TimingSimpleCPU* cpu;
+
+    struct TickEvent : public Event
+    {
+        PacketPtr pkt;
+        TimingSimpleCPU *cpu;
+
+        TickEvent(TimingSimpleCPU *_cpu) : pkt(NULL), cpu(_cpu) {}
+        const char *description() const { return "Timing CPU tick"; }
+        void schedule(PacketPtr _pkt, Tick t);
+    };
+
+    EventFunctionWrapper retryRespEvent;
+};
+
+
+// port that goes out over the mesh
+class ToMeshPort : public TimingCPUMasterPort {
+  public:
+    ToMeshPort(TimingSimpleCPU *_cpu, int idx);
+        /*: TimingCPUMasterPort(
+          _cpu->name() + ".mesh_out_port" + csprintf("[%d]", idx), 
+          idx, _cpu), tickEvent(_cpu), val(false), active(false), 
+          stalledPkt(nullptr)
+    { }*/
+
+    void setVal(bool val);
+    bool getVal() const { return val; }
+    
+    void setActive (bool active) { this->active = active; }
+    bool getActive() const { return active; }
+    
+    void setValIfActive() { if (active) setVal(true); }
+    
+    // check if this port is rdy and the slave port is valid
+    bool checkHandshake();
+    
+    // call tryUnblock() in cpu attach to slave port of this
+    void tryUnblockNeighbor();
+    
+    // buffer pkt and inform we're not ready
+    void failToSend(PacketPtr pkt);
+    
+    // release the pkt and inform we are now ready
+    void beginToSend();
+
+  protected:
+
+    // important to implement/override these from MasterPort class
+    virtual bool recvTimingResp(PacketPtr pkt);
+    virtual void recvReqRetry();
+
+    // the event to put on the event queue when a resp is received
+    struct MOTickEvent : public TickEvent
+    {
+
+        MOTickEvent(TimingSimpleCPU *_cpu)
+            : TickEvent(_cpu) {}
+        void process();
+        const char *description() const { return "Timing CPU to mesh tick"; }
+    };
+
+    MOTickEvent tickEvent;
+    
+    // whether this signal is valid over the mesh net
+    bool val;
+    
+    // whether this port is used and should assert val when packet 
+    // available
+    bool active;
+    
+    // packet stored in register that can't be sent yet due to val/rdy
+    // requires additional enable reg and 2-1 mux
+    PacketPtr stalledPkt;
+    
+};
+
+// similar purpose as TimingCPUPort (derived from master port)
+class TimingCPUSlavePort : public SlavePort {
+  public:
+
+    TimingCPUSlavePort(const std::string& _name, int idx, TimingSimpleCPU* _cpu);
+        /*: SlavePort(_name, _cpu), idx(idx), cpu(_cpu)
+         //, retryRespEvent([this]{ sendRetryResp(); }, name())
+    { }*/
+
+  protected:
+
+    // id in vector port
+    int idx;
+
+    // cpu reference, so we can do things in it on response
+    TimingSimpleCPU* cpu;
+
+    // base for delayed events like processing a request
+    struct TickEvent : public Event
+    {
+        PacketPtr pkt;
+        TimingSimpleCPU *cpu;
+
+        TickEvent(TimingSimpleCPU *_cpu) : pkt(NULL), cpu(_cpu) {}
+        const char *description() const { return "Timing CPU slave tick"; }
+        void schedule(PacketPtr _pkt, Tick t);
+    };
+
+    // potentially want to schedule a retry event in the case that
+    // two things arrive? in the same cycle ??
+    // not sure if would also sched retry in the slave?
+    //EventFunctionWrapper retryRespEvent;
+};
+
+class FromMeshPort : public TimingCPUSlavePort {
+  public:
+    FromMeshPort(TimingSimpleCPU *_cpu, int idx);
+        /*: TimingCPUSlavePort(
+          _cpu->name() + ".mesh_in_port" + csprintf("[%d]", idx), 
+          idx, _cpu), tickEvent(_cpu), rdy(false), active(false)
+    { }*/
+
+    virtual AddrRangeList getAddrRanges() const;
+
+    // get the packet from the port
+    PacketPtr getPacket();
+
+    void setRdy(bool val);
+    bool getRdy() const { return rdy; }
+    
+    void setActive(bool active) { this->active = active; }
+    bool getActive() const { return active; }
+    
+    void setRdyIfActive() { if (active) setRdy(true); }
+    
+    // check val rdy interface
+    bool checkHandshake();
+    
+    // call tryUnblock in the cpu
+    void tryUnblockCPU();
+    
+  protected:
+
+    // important to implement/override these from slave port
+    virtual bool recvTimingReq(PacketPtr pkt);
+    virtual void recvRespRetry();
+    virtual Tick recvAtomic(PacketPtr pkt) { panic("recvAtomic unimpl"); };
+    virtual void recvFunctional(PacketPtr pkt);
+
+    // the event to put on the event queue when a resp is received
+    struct MITickEvent : public TickEvent
+    {
+
+        MITickEvent(TimingSimpleCPU *_cpu)
+            : TickEvent(_cpu) {}
+        void process();
+        const char *description() const { return "Mesh to timing CPU tick"; }
+    };
+
+    MITickEvent tickEvent;
+    
+    // store the most recently received packet (in a single register)
+    PacketPtr recvPkt;
+    
+    // setter, if there is an unused packet already present
+    // we should drop and inform of the lost packet
+    void setPacket(PacketPtr pkt);
+    
+    // whether this port is rdy to recv from the mesh net
+    bool rdy;
+    
+    // this should go high when the core is rdy
+    bool active;
+};
+
+
+#endif
