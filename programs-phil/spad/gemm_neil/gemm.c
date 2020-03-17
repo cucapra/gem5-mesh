e906a448e7a0ab16a12e35dc679d9c1df67f21dd
Neil Adit <neiladit@gmail.com>
2020-03-17 14:23:27 -0400

    first draft for gemm on SIMD

--- programs-phil/spad/gemm_neil/gemm.c
+++ programs-phil/spad/gemm_neil/gemm.c
@@ -6,8 +6,13 @@
 #include "spad.h"
 #include "../../common/bind_defs.h"
 
-// #define VPF
+//  #define VPF
 // #define _VEC
+//#define USE_VECTOR_SIMD
+
+#ifdef USE_VECTOR_SIMD
+#define VPF
+#endif
 
 #ifdef VPF
 #define VEC_PFETCH
@@ -21,7 +26,7 @@
 
 #define BLK_DIM 8
 
-#if defined(_VEC) && defined(VEC_PFETCH)
+#if (defined(_VEC) && defined(VEC_PFETCH)) || defined(USE_VECTOR_SIMD)
 #define REGION_SIZE BLK_DIM
 #define NUM_REGIONS (512/REGION_SIZE)
 #else
@@ -51,6 +56,182 @@ static inline int get_blk_end(int iter, int bound, int blk_dim) {
   }
 }
 
+#ifdef USE_VECTOR_SIMD
+void __attribute__((optimize("-fno-inline"))) 
+gemm_vec(DTYPE *a, DTYPE *b, DTYPE *c, int m, int n, int t, 
+    int m_start, int m_end, int n_start, int n_end, int tid) {
+
+  if (tid == 0) {
+    stats_on();
+  }
+
+  int a_cache_size = REGION_SIZE/2; 
+  int b_cache_size = REGION_SIZE/2;
+  int c_block_size = BLK_DIM*BLK_DIM; 
+
+  int spadRegion = 0;
+  DTYPE *sp_all[4] = {(DTYPE*)getSpAddr(0,0),(DTYPE*)getSpAddr(1,0),(DTYPE*)getSpAddr(2,0),(DTYPE*)getSpAddr(3,0)};
+  DTYPE *spAddr =  sp_all[tid];
+
+  int offset_x, offset_y;
+  int dim_x = 2; //num cpu in a group in x dim
+  int dim_y = 2;
+  int total_cores = dim_x*dim_y;
+
+  int tid_x = tid%dim_x;
+  int tid_y = tid/dim_y;
+
+  offset_x = BLK_DIM*dim_x;
+  offset_y = BLK_DIM*dim_y;
+
+  DTYPE* sp_a = spAddr;
+  DTYPE* sp_b = spAddr + a_cache_size;
+  
+  ISSUE_VINST(fable0);
+  //assuming m_start-m_end is divisble by BLK_DIM
+  for (int i0 = m_start; i0 < m_end ; i0+=offset_y){
+    int i_st = i0 + (tid_y * BLK_DIM);
+    for (int j0 = n_start; j0 < n_end; j0+=offset_x) {
+      int j_st = j0 + (tid_x * BLK_DIM);
+      for (int k = 0; k < t; k++){
+        
+        sp_a = spAddr + spadRegion*REGION_SIZE + 0;
+        sp_b = spAddr + spadRegion*REGION_SIZE + a_cache_size;
+
+        // fetch a in scratchpad
+        for (int i = 0; i <(offset_y/total_cores) ; i++) { 
+          VPREFETCH(sp_a+ i, a + _idx_(k,i0+(i*total_cores),m), 0);
+        }
+    
+        // fetch b in scratchpad
+        for (int j = 0; j <(offset_x/total_cores) ; j++) { 
+          VPREFETCH(sp_b+ j, b + _idx_(k,j0+(j*total_cores),m), 0);
+        }
+
+        ISSUE_VINST(fable1);
+        for (int i = 0; i < BLK_DIM; i++) {
+          for (int j = 0; j < BLK_DIM; j++) {
+            ISSUE_VINST(fable2);
+          }
+          ISSUE_VINST(fable3);
+        }
+
+        spadRegion = (spadRegion + 1) % NUM_REGIONS;
+
+      }
+
+      //store c in DRAM
+      ISSUE_VINST(fable4);
+      for (int i = 0; i < BLK_DIM; i++) {
+          for (int j = 0; j < BLK_DIM; j++) {
+            ISSUE_VINST(fable5);
+            
+          }
+      }
+
+      ISSUE_VINST(fable6);
+    }
+    ISSUE_VINST(fable7);
+  }
+
+  // devec with unique tag
+  DEVEC(devec_0);
+
+  asm volatile("fence\n\t");
+
+  if (tid == 4) {
+    stats_off();
+  }
+
+  return;
+
+
+  //vector region
+
+  DTYPE *sp_a,*sp_b,*sp_c;
+  int tid_x;
+  int tid_y;
+  int i,j;
+  int which_sp_a,sp_offset_a;
+  int which_sp_b,sp_offset_b;
+  int spadRegion;
+  DTYPE a_,b_;
+  int a_cache_size; 
+  int b_cache_size;
+  int c_block_size; 
+
+  int i_st, j_st;
+
+  DTYPE *addr_a,*addr_b;
+
+  fable0:
+    tid_x = tid%dim_x;
+    tid_y = tid/dim_y;
+    spadRegion = 0;
+    DTYPE *sp_all[4] = {(DTYPE*)getSpAddr(0,0),(DTYPE*)getSpAddr(1,0),(DTYPE*)getSpAddr(2,0),(DTYPE*)getSpAddr(3,0)};
+    sp_c = spAddr + NUM_REGIONS*REGION_SIZE;
+
+    a_cache_size = REGION_SIZE/2; 
+    b_cache_size = REGION_SIZE/2;
+    c_block_size = BLK_DIM*BLK_DIM; 
+
+    i_st = m_start + (tid_y * BLK_DIM);
+    j_st = n_start + (tid_x * BLK_DIM);
+
+  fable1:
+    i=0;
+    j=0;
+
+  fable2:
+    which_sp_a = (tid_y*BLK_DIM + i)%total_cores;
+    sp_offset_a = (tid_y*BLK_DIM + i)/total_cores;
+
+    addr_a = sp_all[which_sp_a] + spadRegion*REGION_SIZE + sp_offset_a;
+
+    which_sp_b = (tid_x*BLK_DIM + j)%total_cores;
+    sp_offset_b = (tid_x*BLK_DIM + j)/total_cores;
+    addr_b = sp_all[which_sp_b] + spadRegion*REGION_SIZE + a_cache_size + sp_offset_b;
+    
+
+    LWSPEC(a_, addr_a, 0); 
+    LWSPEC(b_, addr_b, 0);
+
+    sp_c[_idx_(i,j,BLK_DIM)] += a_*b_;
+    j++;
+    spadRegion = (spadRegion + 1) % NUM_REGIONS;
+    REMEM(0); //need to do this collectively for all vector cores if values shared!!
+
+    
+
+  fable3:
+    i++;
+    j=0;
+
+    // need this jump to create loop carry dependencies, but this should be remove later
+    asm volatile goto("j %l[fable2]\n\t"::::fable2);
+
+  fable4:
+    i=0;
+    j=0;
+
+  fable5:
+    STORE_NOACK(sp_c[_idx_(i,j,BLK_DIM)],c + _idx_(i+i_st, j+j_st, n), 0);
+    sp_c[_idx_(i,j,BLK_DIM)]=0;
+
+    // need this jump to create loop carry dependencies, but this should be remove later
+    asm volatile goto("j %l[fable5]\n\t"::::fable5);
+
+  fable6:
+    j_st++;
+
+  fable7:
+    i_st++;
+    j_st=0;
+
+  return;
+}
+
+#else
 void __attribute__((optimize("-fno-inline"))) 
 gemm_vec(DTYPE *a, DTYPE *b, DTYPE *c, int m, int n, int t, 
     int m_start, int m_end, int n_start, int n_end, int tid) {
@@ -185,9 +366,8 @@ gemm_vec(DTYPE *a, DTYPE *b, DTYPE *c, int m, int n, int t,
     }
   }
 
-
-
 }
+#endif
 
 void __attribute__((optimize("-fno-inline")))
 gemm(DTYPE *a, DTYPE *b, DTYPE *c, int m, int n, int t, 
