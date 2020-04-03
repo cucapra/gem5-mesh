17e7b889ca8c0527ccab1302ad16ac0a12a98f20
Edwin Peguero <emp238@cornell.edu>
2020-04-03 18:44:36 -0400

    implemented glueing of scalar/vector assembly files

--- programs-phil/spad/vvadd_simd/glue.py
+++ programs-phil/spad/vvadd_simd/glue.py
@@ -1,53 +1,54 @@
 import itertools
 from enum import Enum, auto
 
-def read_file(filename):
-    file = open(filename, "r")
-    lines = []
-    for line in file.readlines():
-       lines.append(line)
-    return lines
+class VectorParseState(Enum):
+    SIFTING = auto()
+    HEADER = auto()
+    BODY = auto()
 
 header_block_start = "header_block_start"
 header_block_end = "header_block_end"
 vector_body_start = "vector_body_start"
 vector_body_end = "vector_body_end"
-
-class ParseState(Enum):
-    SIFTING = auto()
-    HEADER = auto()
-    BODY = auto()
-
 def copy_vector_code(vector_filename):
     header = []
     body = []
     file = open(vector_filename, "r")
-    state = ParseState.SIFTING
+    state = VectorParseState.SIFTING
     for l in file.readlines():
-        if(state == ParseState.SIFTING):
+        if(state == VectorParseState.SIFTING):
             if(header_block_start in l):
-                state = ParseState.HEADER
+                state = VectorParseState.HEADER
             elif(vector_body_start in l):
-                state = ParseState.BODY
-        elif(state == ParseState.HEADER):
+                state = VectorParseState.BODY
+        elif(state == VectorParseState.HEADER):
             if(header_block_end in l):
-                state = ParseState.SIFTING
+                state = VectorParseState.SIFTING
             else:
                 header.append(l)
-        elif(state == ParseState.BODY):
+        elif(state == VectorParseState.BODY):
             if(vector_body_end in l):
-                state = ParseState.SIFTING
+                state = VectorParseState.SIFTING
             else:
                 body.append(l)
     return "\n".join(header), "\n".join(body)
 
 
-# def combine_with_scalar(combined_filename, scalar_filename, header, body):
-#     file = open(scalar_filename, "r")
-#     combined_file = open(combined_filename, "w+")
 
+vector_header_label = "vector_header_label"
+vector_body_label = "vector_body_label"
+def combine_with_scalar(combined_filename, scalar_filename, header, body):
+    scalar_file = open(scalar_filename, "r")
+    combined_file = open(combined_filename, "w+")
+    for l in scalar_file.readlines():
+        if vector_header_label in l:
+            combined_file.append(header)
+        elif vector_body_label in l:
+            combined_file.append(body)
+        else:
+            combined_file.append(l)
+    return combined_file
 
-header, body = vector_code("vvadd_vector.s")
-
-print("found header:\n{}".format(header))
-print("found body:\n{}".format(body))
+if __name__ == "__main__":
+    header, body = copy_vector_code("vvadd_vector.s")
+    combine_with_scalar("vvadd_combined.s", "vvadd_scalar.s", header, body)
