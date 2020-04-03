3d918517d0550a0e4e00ea4cd0066b55ed2a41c6
Edwin Peguero <emp238@cornell.edu>
2020-04-03 17:59:13 -0400

    'glue.py' can now find vector header/body code blocks in 'vvadd_vector.s'. Next step: combine these with 'vvadd_scalar.s'

--- programs-phil/spad/vvadd_simd/glue.py
+++ programs-phil/spad/vvadd_simd/glue.py
@@ -1,4 +1,5 @@
 import itertools
+from enum import Enum, auto
 
 def read_file(filename):
     file = open(filename, "r")
@@ -7,16 +8,46 @@ def read_file(filename):
        lines.append(line)
     return lines
 
-lines = read_file("vvadd_vector.s")
-def vissue_kernel_body(kernel_start):
-    while_start = next(l for l in lines[kernel_start:]
-                        if is_while_start(l))
-    print(while_start)
+header_block_start = "header_block_start"
+header_block_end = "header_block_end"
+vector_body_start = "vector_body_start"
+vector_body_end = "vector_body_end"
 
-def is_vissue(line):
-    return ".insn" in line and "0x77" in line
+class ParseState(Enum):
+    SIFTING = auto()
+    HEADER = auto()
+    BODY = auto()
 
-def is_while_start(line):
-    return 
+def copy_vector_code(vector_filename):
+    header = []
+    body = []
+    file = open(vector_filename, "r")
+    state = ParseState.SIFTING
+    for l in file.readlines():
+        if(state == ParseState.SIFTING):
+            if(header_block_start in l):
+                state = ParseState.HEADER
+            elif(vector_body_start in l):
+                state = ParseState.BODY
+        elif(state == ParseState.HEADER):
+            if(header_block_end in l):
+                state = ParseState.SIFTING
+            else:
+                header.append(l)
+        elif(state == ParseState.BODY):
+            if(vector_body_end in l):
+                state = ParseState.SIFTING
+            else:
+                body.append(l)
+    return "\n".join(header), "\n".join(body)
 
-vissue_kernel_body(7)
+
+# def combine_with_scalar(combined_filename, scalar_filename, header, body):
+#     file = open(scalar_filename, "r")
+#     combined_file = open(combined_filename, "w+")
+
+
+header, body = vector_code("vvadd_vector.s")
+
+print("found header:\n{}".format(header))
+print("found body:\n{}".format(body))
