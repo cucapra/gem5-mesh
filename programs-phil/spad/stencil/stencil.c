4a786a719f88564f197fc976b95a510dfd2b14b3
pbb59 <pbb59@cornell.edu>
2020-04-06 11:28:55 -0400

    with even larger region size still see exactly the same problem with queueing. i think larger region size reduces the frequency that many elements are queued because fewer _edges_ but still does not remove the edge so the maximum queueing is still the same magnitude and this is probably the metric we care most about

--- programs-phil/spad/stencil/stencil.c
+++ programs-phil/spad/stencil/stencil.c
@@ -46,9 +46,9 @@
 // prefetch sizings
 #if defined(USE_VEC)
 #if defined(LARGE_FRAME)
-#define FRAMES_PER_REGION 8
+#define FRAMES_PER_REGION 16
 #define REGION_SIZE (FILTER_DIM * FILTER_DIM * FRAMES_PER_REGION)
-#define NUM_REGIONS 8
+#define NUM_REGIONS 4
 #define POST_REGION_WORD (REGION_SIZE * NUM_REGIONS)
 #else
 #define REGION_SIZE (FILTER_DIM * FILTER_DIM)
