e1a4b90d13f4b0ed29dc3f83add0211f8b8b216b
pbb59 <pbb59@cornell.edu>
2020-04-16 13:37:19 -0400

    finish comment

--- programs-phil/spad/stencil/main.c
+++ programs-phil/spad/stencil/main.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
   #ifdef REUSE
   int ncols = 12 + (10 * 2); // 12 + factor of 10... wow i.e. 1212
   #else
-  int ncols = 24 + (FILTER_DIM - 1); // factor of 12 + 2... wow i.e. 1214 // vertical needs to be factor of 24
+  int ncols = 24 + (FILTER_DIM - 1); // factor of 12 + 2... wow i.e. 1214 // vertical needs to be factor of 24 + 2... i.e. 1224 + 2 = 1226
   #endif
   
   // parse positional arguments (X Y)
