70759fb589561ab6f91f9313e51530d577fb4334
Edwin Peguero <emp238@cornell.edu>
2020-05-06 16:51:00 -0400

    implemented alternating between gluing vector blocks and copying non-vector blocks after the 'ret' instruction.

--- programs-phil/spad/vvadd_simd/glue.py
+++ programs-phil/spad/vvadd_simd/glue.py
@@ -11,6 +11,9 @@ def is_jump(inst):
     jump_opcodes = ["bgt", "beqz", "bnez", "jr", "j"]
     return inst.split()[0] in jump_opcodes
 
+def strip_whitespace_and_comments(instr):
+    return instr.split("#",1)[0].strip()
+
 def is_whitespace_or_comment(inst):
     stripped = inst.strip()
     return stripped == "" or stripped[0] == "#"
@@ -32,11 +35,10 @@ def remove_whitespace_and_comments(code):
     pass2 = map(lambda l: l.strip(), pass1)
     return list(pass2)
 
-def change_label_prefix(old_prefix, new_prefix, code):
-    return ["."+new_prefix+l[2:]
-                if is_label(l)
-                else l.replace("."+old_prefix,"."+new_prefix)
-                    for l in code]
+def change_label_prefix(old_prefix, new_prefix, instr):
+    return ("."+new_prefix+instr[2:]
+                if is_label(instr)
+                else instr.replace("."+old_prefix,"."+new_prefix))
 
 class VectorParseState(Enum):
     SIFTING = auto()
@@ -110,7 +112,9 @@ class ScalarParseState(Enum):
     AFTER_VECTOR_EPOCH = auto()
     AFTER_DEVEC = auto()
     RETURN_STACK_MANIP = auto()
+    GET_BBS = auto()
     REPLACE_BB_PLACEHOLDERS = auto()
+    GET_NONVEC_BBS = auto()
     FOOTER = auto()
     # SIFT_BB = auto()
     # NON_VECTOR_BB = auto()
@@ -134,18 +138,24 @@ def glue(raw_scalar_code, vector_bbs):
     # [scalar_ret label, "return, jump-like instruction"]
     scalar_ret = []
     # ("return, jump-like instruction", labels pointing to vector blocks]
-    v_bbs = []
+    bbs = []
     # (return statement after labels pointing to vector blocks, rest of file]
     footer = []
 
+    label = None
+    bb = []
     state = ScalarParseState.HEADER
-    for l in scalar_code:
+    for (line_no, l) in scalar_code:
+
+
         if state == ScalarParseState.HEADER:
           if l.strip() == kernel_name + ":":
             header.append(l)
             state = ScalarParseState.BEFORE_VECTOR_EPOCH
           else:
             header.append(l)
+
+
 
         elif state == ScalarParseState.BEFORE_VECTOR_EPOCH:
             if is_VECTOR_EPOCH_inst(l):
@@ -154,6 +164,8 @@ def glue(raw_scalar_code, vector_bbs):
             else:
                 before_VECTOR_EPOCH.append(l)
 
+
+
         elif state == ScalarParseState.AFTER_VECTOR_EPOCH:
             if is_DEVEC(l):
                 after_DEVEC.append(l)
@@ -161,55 +173,109 @@ def glue(raw_scalar_code, vector_bbs):
             else:
                 after_VECTOR_EPOCH.append(l)
 
+
+
         elif state == ScalarParseState.AFTER_DEVEC:
             if scalar_return in l:
                 state = ScalarParseState.RETURN_STACK_MANIP
             else:
                 after_DEVEC.append(l)
 
+
+
         # assumption: label happens right after return jump instr
         elif state == ScalarParseState.RETURN_STACK_MANIP:
             if is_return_inst(l):
                 after_DEVEC.append(l)
-                state = ScalarParseState.REPLACE_BB_PLACEHOLDERS
+                state = ScalarParseState.GET_BBS
             else:
                 scalar_ret.append(l)
 
-        # elif state == ScalarParseState.REPLACE_BB_PLACEHOLDERS:
-        #     if is_label(l):
-        #         prevLabel = l
-        #     elif l in vector_bbs.keys():
-        #         assert(is_label(prevLabel))
-        #         comment = "#Gluing {}-sized block, {}...".format(len(vector_bbs[l]), l)
-        #         print(comment)
-        #         v_bbs.extend([comment, prevLabel] + vector_bbs[l])
-        #     else:
-        #         assert(is_label(prevLabel))
-        #         non_vector_bbs.add(prevLabel)
-        #         prevLabel = None
-        #         state = ScalarParseState.NON_VECTOR_BB
-        #
-        # elif state == ScalarParseState.NON_VECTOR_BB:
-        #     if is_label(l):
-        #         state = ScalarParseState.REPLACE_BB_PLACEHOLDERS
-        #     else:
-        #         non_vector_bbs.add(l)
+
+
+        elif state == ScalarParseState.GET_BBS:
+            if l == "ret":
+                continue
+
+            elif ".size " in l:
+                print("found footer after vector block")
+                bbs.extend(bb)
+                footer.append(l)
+                bb = []
+                state = FOOTER
+
+            elif is_label(l) and label == None:
+                label = l
+                state = ScalarParseState.REPLACE_BB_PLACEHOLDERS
+
+            elif is_label(l) and label != None:
+                print("Warning: passing empty label at line {}".format(line_no))
+                bb.append(l)
+                label = None
+                state = ScalarParseState.REPLACE_BB_PLACEHOLDERS
+
+            else:
+                raise(Exception("expected label at line: {}".format(line_no)))
+
+
+
 
         elif state == ScalarParseState.REPLACE_BB_PLACEHOLDERS:
-            if is_label(l):
-                v_bbs.append(l)
-            elif l in vector_bbs.keys():
-                bb = vector_bbs[l]
-                print("Gluing {}-sized block: {}".format(len(bb), l))
+            is_bb_key = l in vector_bbs.keys()
+
+            # vector block detected: perform gluing
+            if is_bb_key and label != None:
+                bb.extend(vector_bbs[l])
+                print("gluing {} into label {}".format(l, label))
                 start = "#Start {}".format(l)
-                v_bbs.append(start)
-                v_bbs.extend(bb)
-                v_bbs.append("#End {}".format(l))
-            elif is_return_inst(l):
-                state = ScalarParseState.FOOTER
+                bbs.append(label)
+                bbs.append(start)
+                bbs.extend(bb)
+                bbs.append("#End {}".format(l))
+
+                bb = []
+                label = None
+                state = ScalarParseState.GET_BBS
+
+            # non-vector block detected: pass basic block through
+            # empty labels may precede this bb
+            elif not is_bb_key and label != None:
+                print("detected start of non-vector block at label: {}".format(label))
+                bb.append(l)
+                state = ScalarParseState.GET_NONVEC_BBS
+
+            elif is_bb_key and label == None:
+                raise(Exception("found placeholder name for vector block outside of its own label at line {}".format(line_no)))
+
+
+
+
+        elif state == ScalarParseState.GET_NONVEC_BBS:
+            # collect non-vector instruction
+            if not is_label(l) and label != None:
+                bb.append(l)
+
+            # emit non-vector block
+            elif is_label(l) and label != None:
+                print("detected end of non-vector block for label: {}".format(label))
+                bbs.append(label)
+                bbs.extend(bb)
+                bb = []
+                state = ScalarParseState.REPLACE_BB_PLACEHOLDERS
+                label = l
+
+            elif not is_label(l) and label == None:
+                raise(Exception("lost label for non-vector bb at line {}".format(line_no)))
+
+
+
 
         elif state == ScalarParseState.FOOTER:
             footer.append(l)
+
+    # get last basic block, if any
+    bbs.append(label)
+    bbs.extend(bb)
 
     # rearrange components as follows
     return (
@@ -219,14 +285,16 @@ def glue(raw_scalar_code, vector_bbs):
         after_VECTOR_EPOCH[1:] +
         scalar_ret +
         after_DEVEC +
-        v_bbs +
+        bbs +
         footer)
 
 
 
 # assume assembly stripped of whitespace and comments
 def pretty(code):
-    return "\n".join(["\t"+l if not is_label(l) else l for l in code])
+    return "\n".join(["\t"+l
+        if not is_label(l)
+        else l for l in code])
 
 def vector_preprocess(code):
     pass1 = remove_whitespace_and_comments(code)
@@ -234,12 +302,15 @@ def vector_preprocess(code):
                                         (is_jump(inst) and not is_return_inst(inst))), pass1))
 
 def scalar_preprocess(code):
-    pass1 = remove_whitespace_and_comments(code)
-    return change_label_prefix("L", "SCALAR", pass1)
+    with_line_nos = list(enumerate(code))
+    pass1 = apply_transformation(strip_whitespace_and_comments, with_line_nos)
+    pass2 = apply_transformation(lambda instr: change_label_prefix("L", "SCALAR", instr), pass1)
+    return pass2
 
-
-
-
+def apply_transformation(f, code_with_line_no):
+    return [(line_no, f(instr).strip())
+                for (line_no, instr) in code_with_line_no
+                if f(instr).strip() != ""]
 
 if __name__ == "__main__":
     vector_file = open("vvadd_vector.s", "r")
