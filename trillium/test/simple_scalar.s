1db40e549a8c28d48417c7cad19ef74979bf2cdd
Adrian Sampson <adrian@radbox.org>
2020-06-13 10:22:15 -0400

    Actually do some gluing

--- trillium/test/simple_scalar.s
+++ trillium/test/simple_scalar.s
@@ -4,14 +4,22 @@
 somefunc:
 	SCALAR_1
 	.insn i 0x77, 0, x0, a0, 0x401
+.L2:
 	SCALAR_2
+	.insn uj 0x2b, x0, .L14
+	SCALAR_2A
 	trillium vissue_delim return scalar_return
+    ret
     SCALAR_3
+.L4:
 	trillium glue_point vector_init
     SCALAR_4
+.L8:
 	trillium glue_point vector_body
     SCALAR_5
+.L5:
 	trillium glue_point trillium_junk0
     SCALAR_6
+.L13:
 	trillium glue_point vector_return
     SCALAR_7
--- trillium/test/simple_vector.s
+++ trillium/test/simple_vector.s
@@ -5,9 +5,17 @@ somefunc:
     VECTOR_2
 	trillium vissue_delim end
     VECTOR_3
+.L3:
 	trillium vissue_delim begin vector_body
-    VECTOR_4
+    VECTOR_4A
+	.insn i 0x1b, 0x3, x0, a3, 0
+    VECTOR_4B
+	.insn i 0x1b, 0x2, x0, a3, 0
+    VECTOR_4C
+	.insn sb 0x23, 0x5, a4, 0(s0)
+    VECTOR_4D
 	trillium vissue_delim end
     VECTOR_5
+.L2:
 	trillium vissue_delim return vector_return
     VECTOR_6
