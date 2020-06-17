b6291bb32350383aa63c20dcfb077c718a3c5081
Neil Adit <neiladit@gmail.com>
2020-06-17 19:53:28 -0400

    corr on manycorr, get it, get it

---
+++ programs-phil/spad/corr/corr_kernel.h
@@ -0,0 +1,14 @@
+#ifndef __CORR_KERNEL_H__
+#define __CORR_KERNEL_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "pthread_launch.h"
+#include "corr.h"
+#include "spad.h"
+#include "../../common/bind_defs.h"
+
+void corr_vec(int mask);
+
+#endif
