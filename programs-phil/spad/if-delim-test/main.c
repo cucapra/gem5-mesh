adc91c9e36bb94d08a658752d8a72e19894ef54c
Edwin Peguero <emp238@cornell.edu>
2020-06-26 21:11:18 -0400

    adds benchmark test for if delimiter

---
+++ programs-phil/spad/if-delim-test/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <math.h>
+
+#include "spad.h"
+#include "pthread_launch.h"
+#include "template.h"
+
+
+int main(int argc, char *argv[])
+{
+
+  /*--------------------------------------------------------------------
+   * Setup scratchpads
+   *------------------------------------------------------------------*/
+
+  printf("starting\n");
+
+  initScratchpads();
+
+  /*--------------------------------------------------------------------
+  * Get info about manycore
+  *-------------------------------------------------------------------*/
+
+  int cores_x, cores_y;
+  int num_cores = get_dimensions(&cores_x, &cores_y);
+
+  /*--------------------------------------------------------------------
+  * Put the command line arguments into variables
+  *-------------------------------------------------------------------*/
+
+  // default values
+  int n = 1000;
+
+  // parse positional arguments
+  if (argc > 1)
+    n = atoi(argv[1]);
+
+  printf("Problem size is %d\n", n);
+
+  /*--------------------------------------------------------------------
+  * Data initialization
+  *-------------------------------------------------------------------*/
+
+  DTYPE *a_ptr;
+  DTYPE *a = (DTYPE*)malloc_cache_aligned(sizeof(DTYPE), n, (void**)&a_ptr);
+  // DTYPE *a = (DTYPE *)malloc(sizeof(DTYPE) * n);
+
+  // initilaize arrays
+  for(int i=0; i<n; i++){
+    a[i] = i+1;
+  }
+
+  /*--------------------------------------------------------------------
+  * Pack argument for kernel
+  *-------------------------------------------------------------------*/
+
+  // initialize the arguments to send to each device core
+  Kern_Args **kern_args = (Kern_Args **)malloc(sizeof(Kern_Args *) * num_cores);
+
+  for (int y = 0; y < cores_y; y++)
+  {
+    for (int x = 0; x < cores_x; x++)
+    {
+      int i = x + y * cores_x;
+      kern_args[i] = construct_args(a, n, x, y, cores_x, cores_y);
+    }
+  }
+
+  /*--------------------------------------------------------------------
+  * Run the kernel
+  *-------------------------------------------------------------------*/
+
+  printf("Begin kernel on %d cores\n", num_cores);
+  printf("Cores x:%d Cores y:%d\n", cores_x, cores_y);
+  launch_kernel(pthread_kernel, (void **)kern_args, cores_x, cores_y);
+
+/*--------------------------------------------------------------------
+  * Check result and cleanup data
+  *-------------------------------------------------------------------*/
+
+  // if (incorrect){
+  //   printf("[[FAIL]]\n");
+  //   return 1;
+  // }
+
+
+  free(a_ptr);
+  printf("[[SUCCESS]]\n");
+  return 0;
+}
---
+++ programs-phil/spad/if-delim-test/template_kernel.c
@@ -0,0 +1,66 @@
+#include "template_kernel.h"
+
+// #define SCALAR_CORE
+// #define VECTOR_CORE
+
+void tril_template_vec(int mask)
+{
+  //this template uses separate scalar and vector code blocks but they can be interspersed as well as shown here
+  //https://github.com/cucapra/gem5-mesh/wiki/Trilliasm-Language-Overview:-Vector-SIMD-in-C
+
+  #ifdef SCALAR_CORE
+  VECTOR_EPOCH(mask);
+
+  //---------------------------------
+  //scalar core code iterspersed with vissue
+  ISSUE_VINST(init_label); //eg: this block will deal with initila stack manipulation and initilaization of variables
+  //-----------------------------------
+
+
+  /* ISSUE_VINST(if_block_label); */
+  /* #elif defined VECTOR_CORE */
+  /* volatile bool BH; */
+  /* while(BH) { */
+  /*   asm("trillium vissue_delim if_begin if_block"); */
+  /*   if(BH) { */
+  /*     BH = true;  */
+  /*   } */
+  /*   else { */
+  /*     BH = false;  */
+  /*   } */
+  /*   asm("trillium vissue_delim end_if_begin"); */
+  /* } */
+  /* #endif */
+  /* #ifdef SCALAR_CORE */
+
+  //issue stack end portions of vector cores
+  ISSUE_VINST(vector_stack_label);
+
+  // devec with unique tag
+  DEVEC(devec_0);
+
+  //fence for all cores to ensure memory operations have completed
+  asm volatile("fence\n\t");
+
+  asm("trillium vissue_delim return scalar_return"); //return delimiter, delimiters can be of many types
+  return;
+
+  //all the vissue labels below:
+
+  init_label: //this name matches with vissue label name
+    asm("trillium glue_point init"); //name over here "init" matches with delimiter in vector code
+  if_block_label:
+    asm("trillium glue_point if_block");
+  vector_stack_label: 
+    asm("trillium glue_point vector_stack"); //name over here "vector_stack" matches with delimiter in vector code
+
+  #elif defined VECTOR_CORE
+  asm("trillium vissue_delim until_next init"); //until_next delimiter used, name (init) over here same as in glue point above
+  //vector core code
+
+
+  asm("trillium vissue_delim return vector_stack"); //return delimiter
+  return;
+  #endif
+
+}
