19cd8b06df4232969dc59117a3250db9e4ebcb3c
Adrian Sampson <adrian@radbox.org>
2020-06-29 19:45:21 -0400

    Try to make `if-delim-test` make sense
    The old version was pretty crazy; this is an attempt to boil the thing down to
    its essence.
    
    Change-Id: I353878f2d505ce4877ae74b16f411577cc56da16

--- programs-phil/spad/if-delim-test/template_kernel.c
+++ programs-phil/spad/if-delim-test/template_kernel.c
@@ -1,66 +1,62 @@
 #include "template_kernel.h"
 
-// #define SCALAR_CORE
-// #define VECTOR_CORE
-
-void tril_template_vec(int mask)
-{
-  //this template uses separate scalar and vector code blocks but they can be interspersed as well as shown here
-  //https://github.com/cucapra/gem5-mesh/wiki/Trilliasm-Language-Overview:-Vector-SIMD-in-C
-
-  #ifdef SCALAR_CORE
+void tril_template_vec(int mask) {
+#ifdef SCALAR_CORE
   VECTOR_EPOCH(mask);
+#endif
 
-  //---------------------------------
-  //scalar core code iterspersed with vissue
-  ISSUE_VINST(init_label); //eg: this block will deal with initila stack manipulation and initilaization of variables
-  //-----------------------------------
+  // The vector block for initialization.
+#ifdef SCALAR_CORE
+  ISSUE_VINST(init_label);
+#elif defined VECTOR_CORE
+  asm("trillium vissue_delim until_next vector_init");
+  volatile int myResult = 0;
+#endif
 
+  // A loop that issues vector blocks. We use a "black hole" loop to replace
+  // the proper loop on the vector cores.
+#ifdef SCALAR_CORE
+  for (int i = 0; i < 100; ++i) {
+#elif defined VECTOR_CORE
+  volatile int BH;
+  while (BH) {
+#endif
+#ifdef SCALAR_CORE
+    ISSUE_VINST(if_block_label);
+#elif defined VECTOR_CORE
+    asm("trillium vissue_delim if_begin if_block");
+    if (BH) {
+      myResult += 42;
+    } else {
+      myResult += 24;
+    }
+    asm("trillium vissue_delim if_end");
+#endif
+  }
 
-  /* ISSUE_VINST(if_block_label); */
-  /* #elif defined VECTOR_CORE */
-  /* volatile bool BH; */
-  /* while(BH) { */
-  /*   asm("trillium vissue_delim if_begin if_block"); */
-  /*   if(BH) { */
-  /*     BH = true;  */
-  /*   } */
-  /*   else { */
-  /*     BH = false;  */
-  /*   } */
-  /*   asm("trillium vissue_delim end_if_begin"); */
-  /* } */
-  /* #endif */
-  /* #ifdef SCALAR_CORE */
+  // Clean up on the vector cores.
+#ifdef SCALAR_CORE
+  ISSUE_VINST(vector_return_label);
+#elif defined VECTOR_CORE
+  asm("trillium vissue_delim return vector_return");
+  return;
+#endif
 
-  //issue stack end portions of vector cores
-  ISSUE_VINST(vector_stack_label);
-
-  // devec with unique tag
+  // Disband the vector group.
+#ifdef SCALAR_CORE
   DEVEC(devec_0);
-
-  //fence for all cores to ensure memory operations have completed
   asm volatile("fence\n\t");
-
-  asm("trillium vissue_delim return scalar_return"); //return delimiter, delimiters can be of many types
+  asm("trillium vissue_delim return scalar_return");  // XXX is this real???
   return;
+#endif
 
-  //all the vissue labels below:
-
-  init_label: //this name matches with vissue label name
-    asm("trillium glue_point init"); //name over here "init" matches with delimiter in vector code
-  if_block_label:
-    asm("trillium glue_point if_block");
-  vector_stack_label: 
-    asm("trillium glue_point vector_stack"); //name over here "vector_stack" matches with delimiter in vector code
-
-  #elif defined VECTOR_CORE
-  asm("trillium vissue_delim until_next init"); //until_next delimiter used, name (init) over here same as in glue point above
-  //vector core code
-
-
-  asm("trillium vissue_delim return vector_stack"); //return delimiter
-  return;
-  #endif
-
+  // Glue points!
+#ifdef SCALAR_CORE
+init_label:
+  asm("trillium glue_point vector_init");
+if_block_label:
+  asm("trillium glue_point if_block");
+vector_return_label:
+  asm("trillium glue_point vector_return");
+#endif
 }
