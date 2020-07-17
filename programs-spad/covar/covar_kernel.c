9ff51e5d79e26a2ef89d39adf1dde0f557ce262e
pbb59 <pbb59@cornell.edu>
2020-07-17 10:54:12 -0400

    reinstate other vec kernels

--- programs-spad/covar/covar_kernel.c
+++ programs-spad/covar/covar_kernel.c
@@ -19,310 +19,310 @@
 
 #ifdef USE_VEC
 
-// inline void prefetch_mean_frame(DTYPE *data, int i, int j, int *sp, int M) {
-//   VPREFETCH_L(*sp, &data[i * (M+1) + j], 0, MEAN_PREFETCH_LEN, HORIZONTAL);
-//   VPREFETCH_R(*sp, &data[i * (M+1) + j], 0, MEAN_PREFETCH_LEN, HORIZONTAL);
+inline void prefetch_mean_frame(DTYPE *data, int i, int j, int *sp, int M) {
+  VPREFETCH_L(*sp, &data[i * (M+1) + j], 0, MEAN_PREFETCH_LEN, HORIZONTAL);
+  VPREFETCH_R(*sp, &data[i * (M+1) + j], 0, MEAN_PREFETCH_LEN, HORIZONTAL);
 
-//   *sp = *sp + 1;
-//   if (*sp == POST_FRAME_WORD) *sp = 0;
-// }
+  *sp = *sp + 1;
+  if (*sp == POST_FRAME_WORD) *sp = 0;
+}
 
-// // compute each mean across each vector (single dimension)
-// void tril_mean(int mask, DTYPE *mean, DTYPE *data, int N, int M, 
-//     int ptid, int groupId, int numGroups, int vtid) {
+// compute each mean across each vector (single dimension)
+void tril_mean(int mask, DTYPE *mean, DTYPE *data, int N, int M, 
+    int ptid, int groupId, int numGroups, int vtid) {
 
-//   #ifdef SCALAR_CORE
-//   VECTOR_EPOCH(mask);
+  #ifdef SCALAR_CORE
+  VECTOR_EPOCH(mask);
 
-//   int start = ((groupId + 0) * M) / numGroups;
-//   int end   = ((groupId + 1) * M) / numGroups;
+  int start = ((groupId + 0) * M) / numGroups;
+  int end   = ((groupId + 1) * M) / numGroups;
 
-//   // make it a factor of vector group mapping size
-//   start = 1 + roundUp(start, VECTOR_LEN);
-//   end   = 1 + roundUp(end  , VECTOR_LEN);
+  // make it a factor of vector group mapping size
+  start = 1 + roundUp(start, VECTOR_LEN);
+  end   = 1 + roundUp(end  , VECTOR_LEN);
 
-//   ISSUE_VINST(init_label);
-//   #endif
+  ISSUE_VINST(init_label);
+  #endif
 
-//   #ifdef VECTOR_CORE
-//   asm("trillium vissue_delim until_next vector_init");
-//   int start = ((groupId + 0) * M) / numGroups;
-//   start = 1 + roundUp(start, VECTOR_LEN);
-//   // int i = 1;
-//   int j = start + vtid;
-//   DTYPE mean_j = 0.0f;
-//   int sp = 0;
-//   DTYPE *sp_ptr = (DTYPE*)getSpAddr(ptid, 0);
-//   #endif
+  #ifdef VECTOR_CORE
+  asm("trillium vissue_delim until_next vector_init");
+  int start = ((groupId + 0) * M) / numGroups;
+  start = 1 + roundUp(start, VECTOR_LEN);
+  // int i = 1;
+  int j = start + vtid;
+  DTYPE mean_j = 0.0f;
+  int sp = 0;
+  DTYPE *sp_ptr = (DTYPE*)getSpAddr(ptid, 0);
+  #endif
 
-//   #ifdef SCALAR_CORE
+  #ifdef SCALAR_CORE
 
-//   int sp  = 0;
+  int sp  = 0;
 
-//   for (int j = start; j < end; j+=VECTOR_LEN) {
+  for (int j = start; j < end; j+=VECTOR_LEN) {
 
-//     ISSUE_VINST(vec_body_init_label);
+    ISSUE_VINST(vec_body_init_label);
 
-//     // initial round
-//     for (int i = 1; i < 1 + INIT_MEAN_OFFSET; i++) {
-//       prefetch_mean_frame(data, i, start, &sp, M);
-//     }
+    // initial round
+    for (int i = 1; i < 1 + INIT_MEAN_OFFSET; i++) {
+      prefetch_mean_frame(data, i, start, &sp, M);
+    }
 
-//     // steady state
-//     for (int i = 1; i < (N+1); i++) {
-//       prefetch_mean_frame(data, i, j, &sp, M);
-//       ISSUE_VINST(vec_body_label);
-//     }
+    // steady state
+    for (int i = 1; i < (N+1); i++) {
+      prefetch_mean_frame(data, i, j, &sp, M);
+      ISSUE_VINST(vec_body_label);
+    }
 
-//     //cooldown
-//     for (int i = (N+1) - INIT_MEAN_OFFSET; i < (N+1); i++) {
-//       ISSUE_VINST(vec_body_label);
-//     }
+    //cooldown
+    for (int i = (N+1) - INIT_MEAN_OFFSET; i < (N+1); i++) {
+      ISSUE_VINST(vec_body_label);
+    }
 
-//     ISSUE_VINST(vec_body_end_label);
-//   }
-//   #endif
+    ISSUE_VINST(vec_body_end_label);
+  }
+  #endif
 
-//   #ifdef VECTOR_CORE
-//   volatile int BH;
-//   volatile int BHO;
-//   do {
+  #ifdef VECTOR_CORE
+  volatile int BH;
+  volatile int BHO;
+  do {
 
-//     asm("trillium vissue_delim until_next vec_body_init");
+    asm("trillium vissue_delim until_next vec_body_init");
 
-//     do {
-//       asm("trillium vissue_delim if_begin vec_body");
-//       START_FRAME();
-//       mean_j += sp_ptr[sp];
-//       END_FRAME();
-//       asm volatile("nop\n\t");
-//       asm volatile("nop\n\t");
-//       asm volatile("nop\n\t");
-//       sp+=1;
-//       sp = sp % POST_FRAME_WORD;
-//       asm("trillium vissue_delim end at_jump");
-//     } while(BH);
+    do {
+      asm("trillium vissue_delim if_begin vec_body");
+      START_FRAME();
+      mean_j += sp_ptr[sp];
+      END_FRAME();
+      asm volatile("nop\n\t");
+      asm volatile("nop\n\t");
+      asm volatile("nop\n\t");
+      sp+=1;
+      sp = sp % POST_FRAME_WORD;
+      asm("trillium vissue_delim end at_jump");
+    } while(BH);
 
-//     asm("trillium vissue_delim if_begin vec_body_end");
-//     mean_j /= (DTYPE)FLOAT_N;
-//     STORE_NOACK(mean_j, &mean[j], 0);
-//     // i = 1;
-//     j+=VECTOR_LEN;
-//     mean_j = 0.0f;
-//     asm("trillium vissue_delim end at_jump");
+    asm("trillium vissue_delim if_begin vec_body_end");
+    mean_j /= (DTYPE)FLOAT_N;
+    STORE_NOACK(mean_j, &mean[j], 0);
+    // i = 1;
+    j+=VECTOR_LEN;
+    mean_j = 0.0f;
+    asm("trillium vissue_delim end at_jump");
 
-//   } while (BHO);
-//   #endif
-
-
-//   // Clean up on the vector cores.
-// #ifdef SCALAR_CORE
-//   ISSUE_VINST(vector_return_label);
-// #elif defined VECTOR_CORE
-//   asm("trillium vissue_delim return vector_return");
-//   return;
-// #endif
-
-// #ifdef SCALAR_CORE
-//   // devec with unique tag
-//   DEVEC(devec_0);
-
-//   // we are doing lazy store acks, so use this to make sure all stores have commited to memory
-//   asm volatile("fence\n\t");
-//   asm("trillium vissue_delim return scalar_return");  // XXX is this real???
-//   return;
-// #endif
-
-//   // Glue points!
-// #ifdef SCALAR_CORE
-// init_label:
-//   asm("trillium glue_point vector_init");
-// vec_body_init_label:
-//   asm("trillium glue_point vec_body_init");
-// vec_body_label:
-//   asm("trillium glue_point vec_body");
-// vec_body_end_label:
-//   asm("trillium glue_point vec_body_end");
-// vector_return_label:
-//   asm("trillium glue_point vector_return");
-// #endif
-//   // }
-//   // else {
-//   // DTYPE *sp_ptr = (DTYPE*)getSpAddr(ptid, 0);
-//   // for (int j = start + vtid; j < end; j+=VECTOR_LEN) {
-//   //   DTYPE mean_j = 0.0f;
-//   //   for (int i = 1; i < (N+1); i++) {
-//   //     FRAME_START(MEAN_FRAME_SIZE);
-//   //     mean_j += sp_ptr[sp];
-//   //     REMEM(MEAN_FRAME_SIZE);
-//   //     sp++;
-//   //     if (sp == POST_FRAME_WORD) sp = 0;
-//   //   }
-//   //   mean_j /= (DTYPE)FLOAT_N;
-//   //   mean[j] = mean_j;
-//   // }
-
-//   // }
-// }
-
-// inline void prefetch_center_frame(DTYPE *data, DTYPE *mean, int i, int j, int *sp, int M) {
-//   // fetch data
-//   for (int core = 0; core < VECTOR_LEN; core++) {
-//     VPREFETCH_L(*sp, &data[(i + core) * (M+1) + j], core, CENTER_PREFETCH_LEN, VERTICAL);
-//   }
-
-//   *sp = *sp + CENTER_PREFETCH_LEN;
-
-//   // TODO should do more than 1 here
-//   for (int core = 0; core < VECTOR_LEN; core++) {
-//     VPREFETCH_L(*sp, &mean[j], core, CENTER_PREFETCH_LEN, VERTICAL);
-//   }
-//   *sp = *sp + CENTER_PREFETCH_LEN;
-//   if (*sp == POST_FRAME_WORD) *sp = 0;
-// }
-
-// // subract mean from data to "center"
-// void tril_center(int mask, DTYPE *mean, DTYPE *data, int N, int M,
-//     int ptid, int groupId, int numGroups, int vtid) {
-
-//   #ifdef SCALAR_CORE
-//   VECTOR_EPOCH(mask);
-
-//   int start = ((groupId + 0) * N) / numGroups;
-//   int end   = ((groupId + 1) * N) / numGroups;
-
-//   // make it a factor of vector group mapping size
-//   start = 1 + roundUp(start, VECTOR_LEN);
-//   end   = 1 + roundUp(end  , VECTOR_LEN);
-
-//   ISSUE_VINST(init_label);
-//   #endif
-
-//   #ifdef VECTOR_CORE
-//   asm("trillium vissue_delim until_next vector_init");
-//   int start = ((groupId + 0) * N) / numGroups;
-//   start = 1 + roundUp(start, VECTOR_LEN);
-//   int i = start + vtid;
-//   int j = 1;
-//   int sp = 0;
-//   DTYPE* sp_ptr = (DTYPE*)getSpAddr(ptid, 0);
-//   #endif
+  } while (BHO);
+  #endif
 
 
-//   #ifdef SCALAR_CORE
-//   int sp  = 0;
+  // Clean up on the vector cores.
+#ifdef SCALAR_CORE
+  ISSUE_VINST(vector_return_label);
+#elif defined VECTOR_CORE
+  asm("trillium vissue_delim return vector_return");
+  return;
+#endif
 
-//   for (int i = start; i < end; i+=VECTOR_LEN) {
+#ifdef SCALAR_CORE
+  // devec with unique tag
+  DEVEC(devec_0);
 
-//     ISSUE_VINST(vec_body_init_label);
+  // we are doing lazy store acks, so use this to make sure all stores have commited to memory
+  asm volatile("fence\n\t");
+  asm("trillium vissue_delim return scalar_return");  // XXX is this real???
+  return;
+#endif
 
-//     // initial prefetching for the column
-//     for (int j = 1; j < 1 + INIT_CENTER_OFFSET; j++) {
-//       prefetch_center_frame(data, mean, i, j, &sp, M);
-//     }
+  // Glue points!
+#ifdef SCALAR_CORE
+init_label:
+  asm("trillium glue_point vector_init");
+vec_body_init_label:
+  asm("trillium glue_point vec_body_init");
+vec_body_label:
+  asm("trillium glue_point vec_body");
+vec_body_end_label:
+  asm("trillium glue_point vec_body_end");
+vector_return_label:
+  asm("trillium glue_point vector_return");
+#endif
+  // }
+  // else {
+  // DTYPE *sp_ptr = (DTYPE*)getSpAddr(ptid, 0);
+  // for (int j = start + vtid; j < end; j+=VECTOR_LEN) {
+  //   DTYPE mean_j = 0.0f;
+  //   for (int i = 1; i < (N+1); i++) {
+  //     FRAME_START(MEAN_FRAME_SIZE);
+  //     mean_j += sp_ptr[sp];
+  //     REMEM(MEAN_FRAME_SIZE);
+  //     sp++;
+  //     if (sp == POST_FRAME_WORD) sp = 0;
+  //   }
+  //   mean_j /= (DTYPE)FLOAT_N;
+  //   mean[j] = mean_j;
+  // }
 
-//     // steady state
-//     for (int j = 1 + INIT_CENTER_OFFSET; j < (M+1); j++) {
-//       prefetch_center_frame(data, mean, i, j, &sp, M);
-//       ISSUE_VINST(vec_body_label);  
-//     }
+  // }
+}
 
-//     // cooldown
-//     for (int j = (M+1) - INIT_CENTER_OFFSET; j < (M+1); j++) {
-//       ISSUE_VINST(vec_body_label);
-//     }
+inline void prefetch_center_frame(DTYPE *data, DTYPE *mean, int i, int j, int *sp, int M) {
+  // fetch data
+  for (int core = 0; core < VECTOR_LEN; core++) {
+    VPREFETCH_L(*sp, &data[(i + core) * (M+1) + j], core, CENTER_PREFETCH_LEN, VERTICAL);
+  }
 
-//     // tick reset loop
-//     ISSUE_VINST(vec_body_end_label);
+  *sp = *sp + CENTER_PREFETCH_LEN;
 
-//   }
-//   #endif
+  // TODO should do more than 1 here
+  for (int core = 0; core < VECTOR_LEN; core++) {
+    VPREFETCH_L(*sp, &mean[j], core, CENTER_PREFETCH_LEN, VERTICAL);
+  }
+  *sp = *sp + CENTER_PREFETCH_LEN;
+  if (*sp == POST_FRAME_WORD) *sp = 0;
+}
 
-//   #ifdef VECTOR_CORE
-//   volatile int BH;
-//   volatile int BHO;
-//   do {
+// subract mean from data to "center"
+void tril_center(int mask, DTYPE *mean, DTYPE *data, int N, int M,
+    int ptid, int groupId, int numGroups, int vtid) {
 
-//     asm("trillium vissue_delim until_next vec_body_init");
+  #ifdef SCALAR_CORE
+  VECTOR_EPOCH(mask);
 
-//     do {
-//       asm("trillium vissue_delim if_begin vec_body");
-//       START_FRAME();
-//       data[i * (M+1) + j] = sp_ptr[sp + 0] - sp_ptr[sp + 1];
-//       END_FRAME();
-//       sp+=CENTER_FRAME_SIZE;
-//       sp = sp % POST_FRAME_WORD;
-//       j++;
-//       asm("trillium vissue_delim end at_jump");
-//     } while(BH);
+  int start = ((groupId + 0) * N) / numGroups;
+  int end   = ((groupId + 1) * N) / numGroups;
 
-//     asm("trillium vissue_delim if_begin vec_body_end");
-//     j = 1;
-//     i+=VECTOR_LEN;
-//     asm("trillium vissue_delim end at_jump");
+  // make it a factor of vector group mapping size
+  start = 1 + roundUp(start, VECTOR_LEN);
+  end   = 1 + roundUp(end  , VECTOR_LEN);
 
-//   } while (BHO);
-//   #endif
+  ISSUE_VINST(init_label);
+  #endif
 
-//   // Clean up on the vector cores.
-// #ifdef SCALAR_CORE
-//   ISSUE_VINST(vector_return_label);
-// #elif defined VECTOR_CORE
-//   asm("trillium vissue_delim return vector_return");
-//   return;
-// #endif
+  #ifdef VECTOR_CORE
+  asm("trillium vissue_delim until_next vector_init");
+  int start = ((groupId + 0) * N) / numGroups;
+  start = 1 + roundUp(start, VECTOR_LEN);
+  int i = start + vtid;
+  int j = 1;
+  int sp = 0;
+  DTYPE* sp_ptr = (DTYPE*)getSpAddr(ptid, 0);
+  #endif
 
-// #ifdef SCALAR_CORE
-//   // devec with unique tag
-//   DEVEC(devec_0);
 
-//   // we are doing lazy store acks, so use this to make sure all stores have commited to memory
-//   asm volatile("fence\n\t");
-//   asm("trillium vissue_delim return scalar_return");  // XXX is this real???
-//   return;
-// #endif
+  #ifdef SCALAR_CORE
+  int sp  = 0;
 
-//   // Glue points!
-// #ifdef SCALAR_CORE
-// init_label:
-//   asm("trillium glue_point vector_init");
-// vec_body_init_label:
-//   asm("trillium glue_point vec_body_init");
-// vec_body_label:
-//   asm("trillium glue_point vec_body");
-// vec_body_end_label:
-//   asm("trillium glue_point vec_body_end");
-// vector_return_label:
-//   asm("trillium glue_point vector_return");
-// #endif
+  for (int i = start; i < end; i+=VECTOR_LEN) {
 
-//   // }
-//   // else {
-//   // DTYPE *sp_ptr = (DTYPE*)getSpAddr(ptid, 0);
-//   // for (int i = start + vtid; i < end; i+=VECTOR_LEN) {
-//   //   for (int j = 1; j < (M+1); j++) {
-//   //     FRAME_START(CENTER_FRAME_SIZE);
-//   //     data[i * (M+1) + j] = sp_ptr[sp + 0] - sp_ptr[sp + 1];
-//   //     REMEM(CENTER_FRAME_SIZE);
-//   //     sp+=2;
-//   //     if (sp == POST_FRAME_WORD) sp = 0;
-//   //   }
-//   // }
-//   // }
+    ISSUE_VINST(vec_body_init_label);
 
-//   // // unlike gpu version only parallelize outer loop? find if N dimension big enough which prob is
-//   // // or should we use same method as gpu to be fair?
+    // initial prefetching for the column
+    for (int j = 1; j < 1 + INIT_CENTER_OFFSET; j++) {
+      prefetch_center_frame(data, mean, i, j, &sp, M);
+    }
 
-//   // // I think just paralleize outer here? and maybe optimize gpu later?
-//   // int start = ((tid + 0) * N) / dim;
-//   // int end   = ((tid + 1) * N) / dim;
+    // steady state
+    for (int j = 1 + INIT_CENTER_OFFSET; j < (M+1); j++) {
+      prefetch_center_frame(data, mean, i, j, &sp, M);
+      ISSUE_VINST(vec_body_label);  
+    }
 
-//   // for (int i = start + 1; i < end + 1; i++) {
-//   //   for (int j = 1; j < (M+1); j++) {
-//   //     data[i * (M+1) + j] -= mean[j];	
-//   //   }
-//   // }
-// }
+    // cooldown
+    for (int j = (M+1) - INIT_CENTER_OFFSET; j < (M+1); j++) {
+      ISSUE_VINST(vec_body_label);
+    }
+
+    // tick reset loop
+    ISSUE_VINST(vec_body_end_label);
+
+  }
+  #endif
+
+  #ifdef VECTOR_CORE
+  volatile int BH;
+  volatile int BHO;
+  do {
+
+    asm("trillium vissue_delim until_next vec_body_init");
+
+    do {
+      asm("trillium vissue_delim if_begin vec_body");
+      START_FRAME();
+      data[i * (M+1) + j] = sp_ptr[sp + 0] - sp_ptr[sp + 1];
+      END_FRAME();
+      sp+=CENTER_FRAME_SIZE;
+      sp = sp % POST_FRAME_WORD;
+      j++;
+      asm("trillium vissue_delim end at_jump");
+    } while(BH);
+
+    asm("trillium vissue_delim if_begin vec_body_end");
+    j = 1;
+    i+=VECTOR_LEN;
+    asm("trillium vissue_delim end at_jump");
+
+  } while (BHO);
+  #endif
+
+  // Clean up on the vector cores.
+#ifdef SCALAR_CORE
+  ISSUE_VINST(vector_return_label);
+#elif defined VECTOR_CORE
+  asm("trillium vissue_delim return vector_return");
+  return;
+#endif
+
+#ifdef SCALAR_CORE
+  // devec with unique tag
+  DEVEC(devec_0);
+
+  // we are doing lazy store acks, so use this to make sure all stores have commited to memory
+  asm volatile("fence\n\t");
+  asm("trillium vissue_delim return scalar_return");  // XXX is this real???
+  return;
+#endif
+
+  // Glue points!
+#ifdef SCALAR_CORE
+init_label:
+  asm("trillium glue_point vector_init");
+vec_body_init_label:
+  asm("trillium glue_point vec_body_init");
+vec_body_label:
+  asm("trillium glue_point vec_body");
+vec_body_end_label:
+  asm("trillium glue_point vec_body_end");
+vector_return_label:
+  asm("trillium glue_point vector_return");
+#endif
+
+  // }
+  // else {
+  // DTYPE *sp_ptr = (DTYPE*)getSpAddr(ptid, 0);
+  // for (int i = start + vtid; i < end; i+=VECTOR_LEN) {
+  //   for (int j = 1; j < (M+1); j++) {
+  //     FRAME_START(CENTER_FRAME_SIZE);
+  //     data[i * (M+1) + j] = sp_ptr[sp + 0] - sp_ptr[sp + 1];
+  //     REMEM(CENTER_FRAME_SIZE);
+  //     sp+=2;
+  //     if (sp == POST_FRAME_WORD) sp = 0;
+  //   }
+  // }
+  // }
+
+  // // unlike gpu version only parallelize outer loop? find if N dimension big enough which prob is
+  // // or should we use same method as gpu to be fair?
+
+  // // I think just paralleize outer here? and maybe optimize gpu later?
+  // int start = ((tid + 0) * N) / dim;
+  // int end   = ((tid + 1) * N) / dim;
+
+  // for (int i = start + 1; i < end + 1; i++) {
+  //   for (int j = 1; j < (M+1); j++) {
+  //     data[i * (M+1) + j] -= mean[j];	
+  //   }
+  // }
+}
 
 inline void prefetch_covar_frame(DTYPE *data, int i, int j1, int j2, int *sp, int M) {
   // everyone in groups gets the same j1. could share and/or do vertical
