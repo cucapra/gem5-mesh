a0cb4ed3272799bdb8cd772cf54a31083b7e6afc
pbb59 <pbb59@cornell.edu>
2020-08-02 19:16:37 -0400

    relax eq condition

--- programs-spad/fdtd2d/main.c
+++ programs-spad/fdtd2d/main.c
@@ -173,7 +173,7 @@ int main(int argc, char *argv[]) {
     for (int j = 0; j < NY; j++) {
       int idx = i*NY + j;
       // if (hz[idx] != hz_exp[idx]) {
-      if (!float_compare(hz[idx], hz_exp[idx], 0.0001f)) {
+      if (!float_compare(hz[idx], hz_exp[idx], 0.001f)) {
         printf("%f != %f | i %d j %d (%d)\n", hz[idx], hz_exp[idx], i, j, idx);
         printf("[[FAIL]]\n");
         return 1;
