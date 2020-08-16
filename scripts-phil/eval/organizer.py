4cf6c26cbc9b7147f5ad267d922e519015096adb
pbb59 <pbb59@cornell.edu>
2020-08-16 11:41:36 -0400

    only remove gpu series if originally wanted it

--- scripts-phil/eval/organizer.py
+++ scripts-phil/eval/organizer.py
@@ -48,7 +48,7 @@ def sorted_bar_plot(labels, sub_labels, values, ylabel, title, annotate=True, co
       if (v != 0 and not isnan(v)):
         gpu_has_nz = True
 
-  if (not gpu_has_nz):
+  if (not gpu_has_nz and 'GPU' in desired_order):
     desired_order.remove('GPU')
 
   # sort sub bar series
