e4d8e8807fa8a3df6f770db7a4dacbc1a9ced1de
Philip Bedoukian <pbb59@cornell.edu>
2021-04-11 23:28:40 -0400

    3mm missing rvv define

--- programs-spad/3mm/gemm_kernel.h
+++ programs-spad/3mm/gemm_kernel.h
@@ -10,6 +10,10 @@
 #include "bind_defs.h"
 #include "util.h"
 
+#ifdef PER_CORE_SIMD
+#include "riscv_vector.h"
+#endif
+
 void tril_gemm_vec(int mask, DTYPE *a, DTYPE *b, DTYPE *c, int m, int n, int t,
                    int m_start, int m_end, int n_start, int n_end, int vtid_x, int vtid_y, int vtid, int ptid);
 
