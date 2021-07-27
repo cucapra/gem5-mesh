a918e371f6524afb1da84cb0f35692b6bf3880c4
Philip Bedoukian <pbb59@cornell.edu>
2021-07-27 17:35:01 -0400

    update docker

--- docker/enter_docker.sh
+++ docker/enter_docker.sh
@@ -1,3 +1,6 @@
-# script to enter docker container and link to results directory
+# Script to enter docker container and link local gem5 installation
+# Usage: cd <path/to/gem5-mesh>/docker/ && ./enter_docker.sh
 
-docker run -it gem5-mesh
+docker run \
+    -it gem5-mesh \
+    -v $PWD/../:/workspace/gem5-mesh 
