1eac5961ff50668b7cb7fca5f5f335c54cb06430
Philip Bedoukian <pbb59@cornell.edu>
2021-07-27 18:03:13 -0400

    update dockerfile

--- docker/enter_docker.sh
+++ docker/enter_docker.sh
@@ -2,5 +2,6 @@
 # Usage: cd <path/to/gem5-mesh>/docker/ && ./enter_docker.sh
 
 docker run \
-    -it gem5-mesh \
-    -v $PWD/../:/workspace/gem5-mesh 
+    -v $PWD/../:/workspace/gem5-mesh \
+    -w /workspace/gem5-mesh \
+    -it gem5-mesh
