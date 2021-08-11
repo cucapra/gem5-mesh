3ccdb01c098624103b4a03a7ed2608fbdbe3d906
Adrian Sampson <adrian@radbox.org>
2021-08-11 14:52:13 -0400

    Attempt a self-contained Dockerfile
    Change-Id: I06945e76a209d5bb379a595da448ce05eef2a6cb

--- enter_docker.sh
+++ enter_docker.sh
@@ -2,7 +2,5 @@
 # Usage: cd <path/to/gem5-mesh>/docker/ && ./enter_docker.sh
 
 docker run \
-    -v $PWD/../:/workspace/gem5-mesh \
-    -w /workspace/gem5-mesh \
     --user "$(id -u):$(id -g)" \
     -it ghcr.io/cucapra/gem5-mesh:latest
